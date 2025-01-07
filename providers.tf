7852521dd403117303a8370526cd38ad8655916f
Emilio Cisneros <emilio.cisneros.justiniano@gmail.com>
2025-01-07 15:22:14 -0500

    providers added

---
+++ providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = module.tags.default_tags
+  }
+}
+
+module tags {
+  source = "./tags"
+  project_name = var.project_name
+  env = var.env
+  team = var.team
+  project_budget = var.project_budget
+}

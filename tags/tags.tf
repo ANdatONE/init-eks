c90a2b28d56bee181b3be32a0d21265e0f6a093d
Emilio Cisneros <emilio.cisneros.justiniano@gmail.com>
2025-01-07 15:26:25 -0500

    Added tags module

---
+++ tags/tags.tf
@@ -0,0 +1,9 @@
+output "default_tags" {
+  value = {
+    Name = var.project_name
+    env = var.env
+    team = var.team
+    project-name = var.project_name
+    project-budget = var.project_budget
+  }
+}
